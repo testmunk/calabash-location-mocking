32ab2e331f47b778f672a06a06e3f387ba8c1380
Lukasz Fryc <fryc.lukasz@gmail.com>
2015-09-10 12:57:09 +0200

    Added 5s sleep after mocking gps

--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -2,4 +2,6 @@ require 'calabash-android/calabash_steps'
 
 Then /^I set my location to ([-+]?[0-9]*\.?[0-9]+), ([-+]?[0-9]*\.?[0-9]+)$/ do |latitude, longitude|
   set_gps_coordinates(latitude, longitude)
+
+  sleep 5
 end
